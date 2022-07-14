44ab7acfe64eabf6dc85d4d79078867862044f13
Aleksey Nikolaenko <nalex87@gmail.com>
2022-07-13 18:06:41 -0700

    Initial Commit

---
+++ ConwaysLife/ConwaysLifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConwaysLifeApp.swift
+//  ConwaysLife
+//
+//  Created by Aleksey Nikolaenko on 13.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct ConwaysLifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
